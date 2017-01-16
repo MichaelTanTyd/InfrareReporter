123c7ee2f048236c3a98de0db67895b310062036
chenpup <chenpup@gmail.com>
2017-01-16 19:52:18 +0800

    try

--- TempTest20161212V1.m
+++ TempTest20161212V1.m
@@ -6,15 +6,17 @@ clc
 clear
 close all
 
-
-allData = xlsread('test_data.xlsx');
+% 以下数据需要在GUI在文件打开环节打开数据
+allData = xlsread('D:\matlab_gui\InfrareReporter\usr\test_data.xlsx'); 
 [M, N] = size(allData); % M为列坐标，N为横坐标
-%% 作图：温度变化和时间的关系
+%% 作图1：温度变化和时间的关系
 for index = 1:M
     plot(allData(index,3:end)); hold on; grid on;
 end
 legendTxt = num2str(allData(:,1));
 legend(legendTxt,0)
-xlabel('位置值 Pix ');
-ylabel('温度值 ℃');
-title('不同时间段的位置-温度关系图')
+xlabel('位置值（Pix） ');
+ylabel('温度值（℃）');
+title('不同时间段的ROI位置-温度关系图')
+
+%% 作图2：高度方向温度阶梯分布图
