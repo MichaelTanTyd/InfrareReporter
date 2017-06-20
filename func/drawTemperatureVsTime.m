5de6570c86d460407a6ca24833d4cd32d3759afb
chenpup <chenpup@gmail.com>
2017-06-20 20:33:18 +0800

    add temp vs time

--- func/drawTemperatureVsTime.m
+++ func/drawTemperatureVsTime.m
@@ -22,24 +22,31 @@
 addpath(genpath(pwd))
 % load('TemperatureVsTimeData.mat')
 [data,T,S] = xlsread('温度.xlsx');
-ImageData = data(:,4:end-1);
+ImageData = data(:,2:end);
 TimeData = data(:,1);
+
+% ImageData = data(:,4:end);
+% TimeData = data(:,1);
 % Time2Matri()
 Time_Data = Time2Matri(TimeData);
 %% 获取TemperatureVsTimeData
 TemperatureVsTime = [Time_Data,ImageData];
+
+
 % ImageData2 = ImageData;
 
-
-
-
 TG = TemperatureVsTime;
+TG = TG(1:2:end,:);
 TempStart = 5;
 Linewidth = 3; % 线条的粗细
 hight = 7;
 Hour = TG(:,4); 
 Minute = TG(:,5);  
-interval = datenum(0,0,0,Hour,Minute,0);
+Second = TG(:,6);
+interval =datenum(0,0,0,Hour,Minute,Second);
+% interval = datenum(0,0,0,Hour,Minute,1);
+% interval = 1:size(TG,1)
+% hight=1
 % plot(interval,TG(:,hight),opt, 'LineWidth',Linewidth); 
 plot(interval,TG(:,hight),'k-', 'LineWidth',Linewidth);  hold on
 plot(interval,TG(:,hight+1),'g-', 'LineWidth',Linewidth); hold on
@@ -47,9 +54,15 @@ plot(interval,TG(:,hight+2),'b-', 'LineWidth',Linewidth); hold on
 plot(interval,TG(:,hight+3),'r-', 'LineWidth',Linewidth); hold on
 plot(interval,TG(:,hight+4),'y-', 'LineWidth',Linewidth); hold on
 plot(interval,TG(:,hight+5),'m-', 'LineWidth',Linewidth); hold on
-legend('H=100cm','H=90cm','H=80cm','H=70cm','H=60cm','H=50cm')
+plot(interval,TG(:,hight+6),'Color',[0.4 0.5 0.6], 'LineWidth',Linewidth); hold on
+plot(interval,TG(:,hight+7),'Color',[0.4 0.3 0.5], 'LineWidth',Linewidth); hold on
+plot(interval,TG(:,hight+8),'Color',[0.1 0.5 0.6], 'LineWidth',Linewidth); hold on
+
+legend('H=100cm','H=90cm','H=80cm','H=70cm','H=60cm','H=50cm','H=40cm','H=30cm','H=20cm','H=10cm')
+% legend('H=10cm','H=20cm','H=30cm','H=40cm','H=50cm','H=60cm','H=70cm','H=80cm','H=90cm','H=100cm')
 % x = interval;   
-datetick('x','HH:MM'); %需用x作为数组变量
+% datetick('x','HH:MM:SS'); %需用x作为数组变量
+datetick('x',13)
 xlabel('时间'); ylabel('温度 (kpa)');
 set(gca,'ylim',[0 1500],'ytick',0:100:1500);
 title('观测窗不同高度位置在烧结过程中的温度变化趋势')
